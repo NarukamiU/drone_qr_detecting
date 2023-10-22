a19cd733ee350799f977b309aaa5c9247acd7293
Narukami <79750652+NarukamiU@users.noreply.github.com>
2023-10-22 10:36:27 +0900

    Update tello.py
    検出したqrコードの周りを緑の枠で囲うようにしました。

--- tello.py
+++ tello.py
@@ -2,7 +2,7 @@ import cv2
 from pyzbar.pyzbar import decode
 
 # QRコードを読むためのカメラを初期化
-cap = cv2.VideoCapture(4)  # カメラデバイスの番号を指定
+cap = cv2.VideoCapture(0)  # カメラデバイスの番号を指定
 
 while True:
     ret, frame = cap.read()
@@ -32,6 +32,14 @@ while True:
 
             # 新しいコマンド情報を画面に表示
             display_text.append(f"{command}:{value}")
+
+            # QRコードのバウンディングボックスの座標を取得
+            rect_points = obj.polygon
+
+            if len(rect_points) == 4:
+                # 矩形を描画
+                for j in range(4):
+                    cv2.line(frame, tuple(rect_points[j]), tuple(rect_points[(j+1) % 4]), (0, 255, 0), 3)
 
     # 画面にテキストを表示
     text_to_display = ", ".join(display_text)
